mesonium/dino

commit cdb4d77259e6c361aaca64a483a43d7441f4803d

Add support for unencrypted RTP calls to libdino
Co-authored-by: Marvin W <git@larma.de>

libdino/src/service/content_item_store.vala
@@ -29,6 +29,8 @@ public class ContentItemStore : StreamInteractionModule, Object {
         stream_interactor.get_module(FileManager.IDENTITY).received_file.connect(insert_file_transfer);
         stream_interactor.get_module(MessageProcessor.IDENTITY).message_received.connect(announce_message);
         stream_interactor.get_module(MessageProcessor.IDENTITY).message_sent.connect(announce_message);
+        stream_interactor.get_module(Calls.IDENTITY).call_incoming.connect(insert_call);
+        stream_interactor.get_module(Calls.IDENTITY).call_outgoing.connect(insert_call);
     }
 
     public void init(Conversation conversation, ContentItemCollection item_collection) {
@@ -51,7 +53,6 @@ public class ContentItemStore : StreamInteractionModule, Object {
                     Message? message = stream_interactor.get_module(MessageStorage.IDENTITY).get_message_by_id(foreign_id, conversation);
                     if (message != null) {
                         var message_item = new MessageItem(message, conversation, row[db.content_item.id]);
-                        message_item.time = time;
                         items.add(message_item);
                     }
                     break;
@@ -60,6 +61,13 @@ public class ContentItemStore : StreamInteractionModule, Object {
                     if (file_transfer != null) {
                         var file_item = new FileItem(file_transfer, conversation, row[db.content_item.id]);
                         items.add(file_item);
+                    }
+                    break;
+                case 3:
+                    Call? call = stream_interactor.get_module(CallStore.IDENTITY).get_call_by_id(foreign_id);
+                    if (call != null) {
+                        var call_item = new CallItem(call, conversation, row[db.content_item.id]);
+                        items.add(call_item);
                     }
                     break;
             }
@@ -171,6 +179,15 @@ public class ContentItemStore : StreamInteractionModule, Object {
             }
             new_item(item, conversation);
         }
+    }
+
+    private void insert_call(Call call, Conversation conversation) {
+        CallItem item = new CallItem(call, conversation, -1);
+        item.id = db.add_content_item(conversation, call.time, call.local_time, 3, call.id, false);
+        if (collection_conversations.has_key(conversation)) {
+            collection_conversations.get(conversation).insert_item(item);
+        }
+        new_item(item, conversation);
     }
 
     public bool get_item_hide(ContentItem content_item) {
@@ -289,6 +306,20 @@ public class FileItem : ContentItem {
                 return Entities.Message.Marked.WONTSEND;
         }
         assert_not_reached();
+    }
+}
+
+public class CallItem : ContentItem {
+    public const string TYPE = "call";
+
+    public Call call;
+    public Conversation conversation;
+
+    public CallItem(Call call, Conversation conversation, int id) {
+        base(id, TYPE, call.from, call.time, Encryption.NONE, Message.Marked.NONE);
+
+        this.call = call;
+        this.conversation = conversation;
     }
 }
 

libdino/CMakeLists.txt
@@ -15,6 +15,7 @@ SOURCES
     src/dbus/upower.vala
 
     src/entity/account.vala
+    src/entity/call.vala
     src/entity/conversation.vala
     src/entity/encryption.vala
     src/entity/file_transfer.vala
@@ -27,6 +28,8 @@ SOURCES
 
     src/service/avatar_manager.vala
     src/service/blocking_manager.vala
+    src/service/call_store.vala
+    src/service/calls.vala
     src/service/chat_interaction.vala
     src/service/connection_manager.vala
     src/service/content_item_store.vala

libdino/src/application.vala
@@ -39,6 +39,8 @@ public interface Application : GLib.Application {
         AvatarManager.start(stream_interactor, db);
         RosterManager.start(stream_interactor, db);
         FileManager.start(stream_interactor, db);
+        Calls.start(stream_interactor, db);
+        CallStore.start(stream_interactor, db);
         ContentItemStore.start(stream_interactor, db);
         ChatInteraction.start(stream_interactor);
         NotificationEvents.start(stream_interactor);

libdino/src/entity/call.vala
@@ -1,0 +1,126 @@
+using Xmpp;
+
+namespace Dino.Entities {
+
+    public class Call : Object {
+
+        public const bool DIRECTION_OUTGOING = true;
+        public const bool DIRECTION_INCOMING = false;
+
+        public enum State {
+            RINGING,
+            ESTABLISHING,
+            IN_PROGRESS,
+            OTHER_DEVICE_ACCEPTED,
+            ENDED,
+            DECLINED,
+            MISSED,
+            FAILED
+        }
+
+        public int id { get; set; default=-1; }
+        public Account account { get; set; }
+        public Jid counterpart { get; set; }
+        public Jid ourpart { get; set; }
+        public Jid? from {
+            get { return direction == DIRECTION_OUTGOING ? ourpart : counterpart; }
+        }
+        public Jid? to {
+            get { return direction == DIRECTION_OUTGOING ? counterpart : ourpart; }
+        }
+        public bool direction { get; set; }
+        public DateTime time { get; set; }
+        public DateTime local_time { get; set; }
+        public DateTime end_time { get; set; }
+
+        public State state { get; set; }
+
+        private Database? db;
+
+        public Call.from_row(Database db, Qlite.Row row) throws InvalidJidError {
+            this.db = db;
+
+            id = row[db.call.id];
+            account = db.get_account_by_id(row[db.call.account_id]);
+
+            counterpart = db.get_jid_by_id(row[db.call.counterpart_id]);
+            string counterpart_resource = row[db.call.counterpart_resource];
+            if (counterpart_resource != null) counterpart = counterpart.with_resource(counterpart_resource);
+
+            string our_resource = row[db.call.our_resource];
+            if (our_resource != null) {
+                ourpart = account.bare_jid.with_resource(our_resource);
+            } else {
+                ourpart = account.bare_jid;
+            }
+            direction = row[db.call.direction];
+            time = new DateTime.from_unix_utc(row[db.call.time]);
+            local_time = new DateTime.from_unix_utc(row[db.call.local_time]);
+            end_time = new DateTime.from_unix_utc(row[db.call.end_time]);
+            state = (State) row[db.call.state];
+
+            notify.connect(on_update);
+        }
+
+        public void persist(Database db) {
+            if (id != -1) return;
+
+            this.db = db;
+            Qlite.InsertBuilder builder = db.call.insert()
+                    .value(db.call.account_id, account.id)
+                    .value(db.call.counterpart_id, db.get_jid_id(counterpart))
+                    .value(db.call.counterpart_resource, counterpart.resourcepart)
+                    .value(db.call.our_resource, ourpart.resourcepart)
+                    .value(db.call.direction, direction)
+                    .value(db.call.time, (long) time.to_unix())
+                    .value(db.call.local_time, (long) local_time.to_unix())
+                    .value(db.call.state, State.ENDED); // No point in persisting states that can't survive a restart
+            if (end_time != null) {
+                builder.value(db.call.end_time, (long) end_time.to_unix());
+            }
+            id = (int) builder.perform();
+
+            notify.connect(on_update);
+        }
+
+        public bool equals(Call c) {
+            return equals_func(this, c);
+        }
+
+        public static bool equals_func(Call c1, Call c2) {
+            if (c1.id == c2.id) {
+                return true;
+            }
+            return false;
+        }
+
+        public static uint hash_func(Call call) {
+            return (uint)call.id;
+        }
+
+        private void on_update(Object o, ParamSpec sp) {
+            Qlite.UpdateBuilder update_builder = db.call.update().with(db.call.id, "=", id);
+            switch (sp.name) {
+                case "counterpart":
+                    update_builder.set(db.call.counterpart_id, db.get_jid_id(counterpart));
+                    update_builder.set(db.call.counterpart_resource, counterpart.resourcepart); break;
+                case "ourpart":
+                    update_builder.set(db.call.our_resource, ourpart.resourcepart); break;
+                case "direction":
+                    update_builder.set(db.call.direction, direction); break;
+                case "time":
+                    update_builder.set(db.call.time, (long) time.to_unix()); break;
+                case "local-time":
+                    update_builder.set(db.call.local_time, (long) local_time.to_unix()); break;
+                case "end-time":
+                    update_builder.set(db.call.end_time, (long) end_time.to_unix()); break;
+                case "state":
+                    // No point in persisting states that can't survive a restart
+                    if (state == State.RINGING || state == State.ESTABLISHING || state == State.IN_PROGRESS) return;
+                    update_builder.set(db.call.state, state);
+                    break;
+            }
+            update_builder.perform();
+        }
+    }
+}

libdino/src/plugin/interfaces.vala
@@ -84,6 +84,31 @@ public abstract interface ConversationAdditionPopulator : ConversationItemPopula
     public virtual void populate_timespan(Conversation conversation, DateTime from, DateTime to) { }
 }
 
+public abstract interface VideoCallPlugin : Object {
+    // Video widget
+    public abstract VideoCallWidget? create_widget(WidgetType type);
+
+    // Devices
+    public signal void devices_changed(string media, bool incoming);
+    public abstract Gee.List<MediaDevice> get_devices(string media, bool incoming);
+    public abstract MediaDevice? get_device(Xmpp.Xep.JingleRtp.Stream stream, bool incoming);
+    public abstract void set_pause(Xmpp.Xep.JingleRtp.Stream stream, bool pause);
+    public abstract void set_device(Xmpp.Xep.JingleRtp.Stream stream, MediaDevice? device);
+}
+
+public abstract interface VideoCallWidget : Object {
+    public signal void resolution_changed(uint width, uint height);
+    public abstract void display_stream(Xmpp.Xep.JingleRtp.Stream stream); // TODO: Multi participant
+    public abstract void display_device(MediaDevice device);
+    public abstract void detach();
+}
+
+public abstract interface MediaDevice : Object {
+    public abstract string id { get; }
+    public abstract string display_name { get; }
+    public abstract string detail_name { get; }
+}
+
 public abstract interface NotificationPopulator : Object {
     public abstract string id { get; }
     public abstract void init(Conversation conversation, NotificationCollection summary, WidgetType type);

libdino/src/plugin/loader.vala
@@ -26,7 +26,7 @@ public class Loader : Object {
         this.search_paths = app.search_path_generator.get_plugin_paths();
     }
 
-    public void loadAll() throws Error {
+    public void load_all() throws Error {
         if (Module.supported() == false) {
             throw new Error(-1, 0, "Plugins are not supported");
         }

libdino/src/plugin/registry.vala
@@ -12,6 +12,7 @@ public class Registry {
     internal Gee.Collection<ConversationTitlebarEntry> conversation_titlebar_entries = new Gee.TreeSet<ConversationTitlebarEntry>((a, b) => {
         return (int)(a.order - b.order);
     });
+    public VideoCallPlugin? video_call_plugin;
 
     public bool register_encryption_list_entry(EncryptionListEntry entry) {
         lock(encryption_list_entries) {

libdino/src/service/call_store.vala
@@ -1,0 +1,61 @@
+using Xmpp;
+using Gee;
+using Qlite;
+
+using Dino.Entities;
+
+namespace Dino {
+
+    public class CallStore : StreamInteractionModule, Object {
+        public static ModuleIdentity<CallStore> IDENTITY = new ModuleIdentity<CallStore>("call_store");
+        public string id { get { return IDENTITY.id; } }
+
+        private StreamInteractor stream_interactor;
+        private Database db;
+
+        private WeakMap<int, Call> calls_by_db_id = new WeakMap<int, Call>();
+
+        public static void start(StreamInteractor stream_interactor, Database db) {
+            CallStore m = new CallStore(stream_interactor, db);
+            stream_interactor.add_module(m);
+        }
+
+        private CallStore(StreamInteractor stream_interactor, Database db) {
+            this.stream_interactor = stream_interactor;
+            this.db = db;
+        }
+
+        public void add_call(Call call, Conversation conversation) {
+            call.persist(db);
+            cache_call(call);
+        }
+
+        public Call? get_call_by_id(int id) {
+            Call? call = calls_by_db_id[id];
+            if (call != null) {
+                return call;
+            }
+
+            RowOption row_option = db.call.select().with(db.call.id, "=", id).row();
+
+            return create_call_from_row_opt(row_option);
+        }
+
+        private Call? create_call_from_row_opt(RowOption row_opt) {
+            if (!row_opt.is_present()) return null;
+
+            try {
+                Call call = new Call.from_row(db, row_opt.inner);
+                cache_call(call);
+                return call;
+            } catch (InvalidJidError e) {
+                warning("Got message with invalid Jid: %s", e.message);
+            }
+            return null;
+        }
+
+        private void cache_call(Call call) {
+            calls_by_db_id[call.id] = call;
+        }
+    }
+}

commit 173cf0b874a8366cc97562e2e23d6be708839b03

Display corrected messages with original message time
Regression from cdb4d77

libdino/src/service/content_item_store.vala
@@ -53,6 +53,7 @@ public class ContentItemStore : StreamInteractionModule, Object {
                     Message? message = stream_interactor.get_module(MessageStorage.IDENTITY).get_message_by_id(foreign_id, conversation);
                     if (message != null) {
                         var message_item = new MessageItem(message, conversation, row[db.content_item.id]);
+                        message_item.time = time; // In case of message corrections, the original time should be used
                         items.add(message_item);
                     }
                     break;